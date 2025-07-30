6a2121bb19552528637ab082600316ce0c126db6
eomjinyoung <jinyoung.eom@gmail.com>
2025-07-31 04:39:30 +0900

    .

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,23 +1,13 @@
 package org.example.myapp;
 
-import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-/**
- * 이 클래스는 한밭대학교 특강 실습 예제를 위한 첫 번째 클래스입니다.<br>
- * <code>App</code> 클래스!
- */
+@EnableAutoConfiguration
 public class App {
 
-  /**
-   * 프로그램의 진입점(entry point) 메서드입니다. 콘솔에 "Hello!" 메시지를 출력합니다.
-   *
-   * @param args 명령행 인수 배열 (이 프로그램에서는 사용되지 않음)
-   */
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("이름? ");
-    String name = scanner.nextLine();
-    System.out.printf("안녕하세요 %s 님!\n", name);
-    scanner.close();
+    SpringApplication.run(App.class, args);
+    System.out.println("스프링부트 서버 시작!");
   }
 }
