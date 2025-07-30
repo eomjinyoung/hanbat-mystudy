67848b129b9e6f438959a1fc53152f399a474e77
eomjinyoung <jinyoung.eom@gmail.com>
2025-07-31 06:16:00 +0900

    .

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,9 +1,17 @@
 package org.example.myapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+// 스프링 부트를 실행할 수 있도록 기본 정보를 자동 설정
 @EnableAutoConfiguration
+
+// 현재 클래스와 같은 패키지 및 하위 패키지를 다 뒤져서 컴포넌트를 찾아 자동 생성하라는 설정
+// => @Component, @RestController, @Controller, @Service, @Repository 등이 붙은 클래스를 찾는다.
+@ComponentScan
+*/
+@SpringBootApplication // = @SpringBootConfiguratin + @EnableAutoConfiguration + @ComponentScan
 public class App {
 
   public static void main(String[] args) {
